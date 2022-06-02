17d46073ba7927e6c95674661b7271c5f0919ac6
iamqaasim <iamqaasim@gmail.com>
2022-06-02 15:28:56 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point   
+ * main - Entry point
+ *
  * Return: 1
  */
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(1, ch, 59);
 	return (1);
 }
