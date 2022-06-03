4ce8783225d3375bdd086b6adc3944dad9ac75e3
iamqaasim <iamqaasim@gmail.com>
2022-06-03 13:54:04 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 
 /**
-* more headers goes there
+* main - something
 *
-* betty style doc for function main goes there 
+* betty style doc for function main goes there
 *
-* This program will assign a random number to the variable n each time it is executed
+* Program assign a random number to the variable n each time it is executed
 *
-* The variable n will store a different value every time you will run this program 
+* Var n will store a different value every time you will run this program
+*
+* Return: 0 
 */
 
 int main(void)
@@ -19,17 +21,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n < 0)
+	if (n < 0)
 	{
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
