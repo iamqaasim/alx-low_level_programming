213c170d67f2d7ff7bca4d56cc7ba01951ad7f4b
iamqaasim <iamqaasim@gmail.com>
2022-06-03 09:18:23 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there
+*
+*  betty style doc for function main goes there 
+*
+*  This program will assign a random number to the variable n each time it is executed
+*
+*  The variable n will store a different value every time you will run this program */
+
+int main(void)
+
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n<0)
+	{
+		printf("is negative\n");
+	}
+	else if (n>0)
+	{
+		printf("is positive\n");
+	}
+	else
+	{
+		print("is zero \n");
+	}
+	return (0);
+
+}
