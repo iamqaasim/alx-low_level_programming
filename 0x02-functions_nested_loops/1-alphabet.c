e13d8b159fdca26526c5b89692ba40826d4447ce
iamqaasim <iamqaasim@gmail.com>
2022-06-07 15:39:28 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print alphabet using your _putchar function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	int i;
+	
+	for (i = 0; i <= 'z', i++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
