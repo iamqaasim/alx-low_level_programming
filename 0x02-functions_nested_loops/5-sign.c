94def9179516fa439fb60ff295489780d3d8d69a
iamqaasim <iamqaasim@gmail.com>
2022-06-07 18:06:05 -0700

    My first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 #include "main.h"
-  
+
 /**
  * print_sign - print sign
  * 
@@ -10,15 +10,16 @@
 
 int print_sign(int n)
 {
-        if (n > 0)
-        {
+	if (n > 0)
+	{
 		_putchar('+');
-                return (+1);
-        }
+		return (1);
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		return (1);
 	}
-        return (0);
-} 
+	_putchar('0')
+	return (0);
+}
