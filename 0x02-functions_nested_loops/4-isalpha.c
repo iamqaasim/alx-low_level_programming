38e12e4e2d59e99ca8a5fa0614aee0797f3c244c
iamqaasim <iamqaasim@gmail.com>
2022-06-07 17:48:10 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+  
+/**
+ * _isalpha - checks for lower case and upper case
+ * @c - arguement for _islower
+ *
+ * Return: 1 if lower case 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        {
+                return (1);
+        }
+        return (0);
+}
