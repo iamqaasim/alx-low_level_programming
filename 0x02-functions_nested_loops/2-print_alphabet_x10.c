90fadb1e227524a3d7c0c3caf8e9fd5b9c20d188
iamqaasim <iamqaasim@gmail.com>
2022-06-07 17:15:04 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,5 @@
 #include "main.h"
-  
+
 /**
  * print_alphabet_x10 - print alphabet using your _putchar function
  *
@@ -9,15 +9,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	for (i = '0'; i == '9'; i++)
-	{	
+
+	for (i = '0'; i <= '9'; i++)
+	{
 		char c;
-		
+
 		for (c = 'a'; c <= 'z'; c++)
-        	{
-                	_putchar(c);
-        	}
+		{
+			_putchar(c);
+		}
 		_putchar('\n');
 	}
 }
