61e7bb14415a1c082f0160fd2a8a5bbb9efcdd0b
iamqaasim <iamqaasim@gmail.com>
2022-06-07 17:58:14 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+  
+/**
+ * print_sign - print sign
+ * 
+ * @n - arguement for _islower
+ *
+ * Return: 1 if lower case 0 otherwise
+ */
+
+int print_sign(int n)
+{
+        if (n > 0)
+        {
+                return (1);
+        }
+	else if (n < 0)
+	{
+		return (-1);
+	}
+        return (0);
+} 
