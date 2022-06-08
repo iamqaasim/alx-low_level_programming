e934a3ba1945519ff3fc97cf17de6f10148dc4a9
iamqaasim <iamqaasim@gmail.com>
2022-06-07 18:21:47 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs() - computes absolute value
+ *
+ * Return: 0 for success
+ */
+
+int _abs(int)
+}
+	if (int < 0)
+	{
+		int value = int * -1
+		
+		_putchar(value);
+	}
+	else
+	{
+		_putchar(int);
+	}
+	return (0);
+}
