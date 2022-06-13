ff17a31e47ec734d45386fef977acf035d4d9547
iamqaasim <iamqaasim@gmail.com>
2022-06-13 11:39:07 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints a string
+ * @a: string
+ * @n: amount of eleents we want to print
+ * Return: 0 for success
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i = n-1; i++)
+	{
+		_putchar(*(a + i));
+	}
+	_putchar('\n');
+}
