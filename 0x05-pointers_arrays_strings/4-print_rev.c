28db135b5dafca5afb25e4551e77527652ee2a59
iamqaasim <iamqaasim@gmail.com>
2022-06-13 09:50:37 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ *
+ * Return: 0 for success
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+        for (count = 0; *(s + count) != ('\0'); count++)
+        {
+        }
+
+	for (count; >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
