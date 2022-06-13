7f6514a2f01cf62ae5481c245867ead2267b7bc7
iamqaasim <iamqaasim@gmail.com>
2022-06-13 12:02:16 -0700

    My first commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
-		_putchar(',');
-		_putchar(' ');
+		printf("%d",a[i]);
+		printf(", ");
 	}
 	_putchar('\n');
 }
