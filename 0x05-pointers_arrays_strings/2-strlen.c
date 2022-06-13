a8c22f8b0f1307f4c427d99ef204b9ed5fef9b3a
iamqaasim <iamqaasim@gmail.com>
2022-06-13 08:46:48 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: char argument
+ *
+ * Return: 0 for success
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0;*(s + i) = '\0';i++)
+	{
+	
+	}
+	return (i)
+}
