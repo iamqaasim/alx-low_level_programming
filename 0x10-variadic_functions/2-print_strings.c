ca3f7b184c08b7253fa17a875073d379a7bcc21d
Iamqaasim <iamqaasim@gmail.com>
2022-08-19 22:08:04 +0200

    My first commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,7 @@
 /**
  * print_strings - Prints strings, followed by a new line.
  * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
+ * @n: The number of strings passed to the function. 
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
